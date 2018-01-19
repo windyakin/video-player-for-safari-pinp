ea1928fdc549020dbad36747cc69fbc7a5cab427
windyakin <windyakin@gmail.com>
2018-01-19 19:57:05 +0900

    WIP

--- components/script.js
+++ components/script.js
@@ -1,14 +1,32 @@
 (function(window, document, undefined) {
+  var playlist = new Array();
+  var nowPlayingNumber = 0;
   var player = document.getElementById('video-player');
   var droparea = document.getElementById('video-droparea');
+
+  var playVideo = function(number) {
+    var URL = window.URL || window.webkitURL;
+    player.src = URL.createObjectURL(playlist[number]);
+  };
 
   droparea.addEventListener('drop', function(event) {
     event.preventDefault();
     droparea.classList.remove('video-dropover');
     droparea.classList.add('video-dropped');
-    var URL = window.URL || window.webkitURL;
-    var file = event.dataTransfer.files[0];
-    player.src = URL.createObjectURL(file);
+    for (var i = 0; i < event.dataTransfer.files.length; i++) {
+      playlist.push(event.dataTransfer.files[i]);
+    }
+    if (playlist.length == 1) {
+      playVideo(0);
+    }
+  });
+
+  player.addEventListener('ended', function(event) {
+    nowPlayingNumber += 1;
+    if (playlist.length == nowPlayingNumber) {
+      nowPlayingNumber = 0;
+    }
+    playVideo(nowPlayingNumber);
   });
 
   droparea.addEventListener('dragover', function(event) {
